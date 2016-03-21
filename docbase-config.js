f0fdb0846fd5ae84afbe4650a79de2b4e3aff7ba
Farhan Chauhan <farhan687@gmail.com>
2016-03-21 23:24:24 +0530

    Add spa

---
+++ docbase-config.js
@@ -0,0 +1,240 @@
+var docbaseConfig = {
+  "method": "github",
+  "map": {
+    "file": "map.json",
+    "path": ""
+  },
+  "generic": {
+    "baseurl": "",
+    "path": ""
+  },
+  "file": {
+    "path": ""
+  },
+  "github": {
+    "user": "farhan687",
+    "repo": "redis",
+    "path": "src",
+    "branch": "master",
+    "access_token": "945751d3f595b888024dbcab62b339134d2cac9f"
+  },
+  "indexHtml": "html/main.html",
+  "flatdocHtml": "../bower_components/docbase/html/flatdoc.html",
+  "html5mode": false,
+  "versions": {
+    "alpha": [
+      {
+        "label": "commands",
+        "name": "commands",
+        "files": [
+          {
+            "name": "append",
+            "label": "append"
+          },
+          {
+            "name": "auth",
+            "label": "auth"
+          },
+          {
+            "name": "bgrewriteaof",
+            "label": "bgrewriteaof"
+          },
+          {
+            "name": "bgsave",
+            "label": "bgsave"
+          },
+          {
+            "name": "bitcount",
+            "label": "bitcount"
+          },
+          {
+            "name": "bitop",
+            "label": "bitop"
+          },
+          {
+            "name": "bitpos",
+            "label": "bitpos"
+          },
+          {
+            "name": "blpop",
+            "label": "blpop"
+          },
+          {
+            "name": "brpop",
+            "label": "brpop"
+          },
+          {
+            "name": "brpoplpush",
+            "label": "brpoplpush"
+          },
+          {
+            "name": "client-getname",
+            "label": "client-getname"
+          },
+          {
+            "name": "client-kill",
+            "label": "client-kill"
+          },
+          {
+            "name": "client-list",
+            "label": "client-list"
+          },
+          {
+            "name": "client-pause",
+            "label": "client-pause"
+          },
+          {
+            "name": "client-reply",
+            "label": "client-reply"
+          },
+          {
+            "name": "client-setname",
+            "label": "client-setname"
+          },
+          {
+            "name": "cluster-addslots",
+            "label": "cluster-addslots"
+          },
+          {
+            "name": "cluster-count-failure-reports",
+            "label": "cluster-count-failure-reports"
+          },
+          {
+            "name": "cluster-countkeysinslot",
+            "label": "cluster-countkeysinslot"
+          },
+          {
+            "name": "cluster-delslots",
+            "label": "cluster-delslots"
+          },
+          {
+            "name": "cluster-failover",
+            "label": "cluster-failover"
+          },
+          {
+            "label": "index",
+            "name": "index"
+          }
+        ]
+      },
+      {
+        "label": "topics",
+        "name": "topics",
+        "files": [
+          {
+            "name": "admin",
+            "label": "admin"
+          },
+          {
+            "name": "benchmarks",
+            "label": "benchmarks"
+          },
+          {
+            "name": "clients",
+            "label": "clients"
+          },
+          {
+            "name": "cluster-spec",
+            "label": "cluster-spec"
+          },
+          {
+            "name": "cluster-tutorial",
+            "label": "cluster-tutorial"
+          },
+          {
+            "name": "config",
+            "label": "config"
+          },
+          {
+            "name": "data-types-intro",
+            "label": "data-types-intro"
+          },
+          {
+            "name": "data-types",
+            "label": "data-types"
+          },
+          {
+            "name": "debugging",
+            "label": "debugging"
+          },
+          {
+            "name": "distlock",
+            "label": "distlock"
+          },
+          {
+            "name": "encryption",
+            "label": "encryption"
+          },
+          {
+            "name": "faq",
+            "label": "faq"
+          },
+          {
+            "name": "indexes",
+            "label": "indexes"
+          },
+          {
+            "name": "internals-eventlib",
+            "label": "internals-eventlib"
+          },
+          {
+            "name": "internals-rediseventlib",
+            "label": "internals-rediseventlib"
+          },
+          {
+            "name": "internals-sds",
+            "label": "internals-sds"
+          },
+          {
+            "name": "internals-vm",
+            "label": "internals-vm"
+          },
+          {
+            "name": "internals",
+            "label": "internals"
+          },
+          {
+            "name": "introduction",
+            "label": "introduction"
+          },
+          {
+            "name": "latency-monitor",
+            "label": "latency-monitor"
+          },
+          {
+            "name": "latency",
+            "label": "latency"
+          },
+          {
+            "name": "ldb",
+            "label": "ldb"
+          },
+          {
+            "name": "license",
+            "label": "license"
+          },
+          {
+            "name": "lru-cache",
+            "label": "lru-cache"
+          },
+          {
+            "name": "mass-insert",
+            "label": "mass-insert"
+          },
+          {
+            "name": "memory-optimization",
+            "label": "memory-optimization"
+          },
+          {
+            "name": "notifications",
+            "label": "notifications"
+          },
+          {
+            "label": "index",
+            "name": "index"
+          }
+        ]
+      }
+    ]
+  }
+};
