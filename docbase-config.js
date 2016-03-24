a27089ba902e7e98140e7128a0e8966cc60c6ad3
Farhan Chauhan <farhan687@gmail.com>
2016-03-24 12:29:21 +0530

    Docbase applied

---
+++ docbase-config.js
@@ -0,0 +1,49 @@
+var docbaseConfig = {
+  "method": "github",
+  "map": {
+    "file": "map.json",
+    "path": ""
+  },
+  "generic": {
+    "baseurl": "",
+    "path": ""
+  },
+  "file": {
+    "path": ""
+  },
+  "github": {
+    "user": "farhan687",
+    "repo": "redis",
+    "path": "source",
+    "branch": "master",
+    "access_token": "MzNlYmQ5NzA3NWQyODAyZGZiZDE2ZjBiZjkzMGViZWI0NmU5MDVjNQ=="
+  },
+  "indexHtml": "./html/main.html",
+  "flatdocHtml": "./bower_components/docbase/html/flatdoc.html",
+  "html5mode": false,
+  "versions" : {
+    "v1": [{
+      "name": "sample",
+      "label": "Sample Label",
+      "files": [{
+        "name": "sample1",
+        "label": "Sample 1 Doc"
+      },]
+    }, {
+      "name": "howtostart",
+      "label": "How to start",
+      "files": [{
+        "name": "starting",
+        "label": "Starting with docbase"
+      }]
+    }],
+    "v2": [{
+      "name": "sample",
+      "label": "Sample Label",
+      "files": [{
+        "name": "sample1",
+        "label": "Sample 2 Doc"
+      }]
+    }]
+  }
+}
